f21bef3769c2a7306df30af6e99dd3a8bfdccf0d
sixthgear <sixthgear@sixthgear.ca>
2011-09-26 11:59:44 -0700

    inconsequential collisions change

--- collision.py
+++ collision.py
@@ -1,4 +1,9 @@
+import vector
 import shapes
+
+SQRT2 = 1.414213562373095
+
+### detection
 
 def point_within_circle(point, circle):    
     return (circle.x - point.x) ** 2 + (circle.y - point.y) ** 2 <= circle.radius ** 2
@@ -26,3 +31,40 @@ def rect_vs_circle(rect, circle):
 #     if rect_in.x + rect_in.width > rect_out.x + rect_out.width: return False
 #     if rect_in.y + rect_in.height > rect_out.y + rect_out.height: return False
 #     return True
+
+#### response
+
+def resp_circle_vs_full(circle, node):
+    """
+    Finds all nodes currently in contact with the player.
+    """
+    halfquad = node.rect.width // 2
+    quad_center = vector.Vec2d(node.rect.x + halfquad, node.rect.y + halfquad)
+    delta = quad_center - vector.Vec2d(circle.x, circle.y)
+    rest = vector.Vec2d()
+    rest_dist = 0
+
+    if abs(delta.x) > abs(delta.y):
+        rest_dist = abs(delta.x) - (circle.radius + halfquad)
+        if delta.x < 0: 
+            rest.x = -rest_dist
+        else:
+            rest.x = rest_dist
+
+    elif abs(delta.x) < abs(delta.y):
+        rest_dist = abs(delta.y) - (circle.radius + halfquad)
+        if delta.y < 0:
+            rest.y = -rest_dist
+        else:
+            rest.y = rest_dist
+
+    else:                
+        # diagonal collision                
+        rest_dist = delta.magnitude - (circle.radius + halfquad * SQRT2)
+        rest = delta.normal * -rest_dist
+        print 'CORNER CASE', rest
+            
+    return rest
+
+    # n = len(self.terrain.collide_circle(self.player.shape))
+    # if n: print n
